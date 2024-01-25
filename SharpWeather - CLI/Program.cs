94167b04c554b4fc0bbae3e3b00db915cb1d79f1
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-01-24 17:24:19 -0800

    Add SharpWeather CLI
    very cool ig includes api key

---
+++ SharpWeather - CLI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+Console.Write("Enter the name of the city: ");
+string city = Console.ReadLine();
+
+using (var httpClient = new HttpClient())
+{
+    using (var response = await httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=853c934c10849992f194eb00c697df74&units=metric"))
+    {
+        string apiResponse = await response.Content.ReadAsStringAsync();
+        if (apiResponse.Contains("city not found"))
+        {
+            Console.WriteLine($"Weather not found for {city}");
+        }
+        else
+        {
+            var weatherData = JsonConvert.DeserializeObject<WeatherData>(apiResponse);
+
+            Console.WriteLine($"Temperature in {weatherData.Name}: {weatherData.Main.Temp}°C");
+            Console.WriteLine($"Weather: {weatherData.Weather[0].Description}");
+            Console.WriteLine($"Wind speed: {weatherData.Wind.Speed} m/s");
+        }
+    }
+}
---
+++ SharpWeather - CLI/WeatherData.cs
@@ -0,0 +1,22 @@
+﻿public class WeatherData
+{
+    public string Name { get; set; }
+    public Main Main { get; set; }
+    public Weather[] Weather { get; set; }
+    public Wind Wind { get; set; }
+}
+
+public class Main
+{
+    public double Temp { get; set; }
+}
+
+public class Weather
+{
+    public string Description { get; set; }
+}
+
+public class Wind
+{
+    public double Speed { get; set; }
+}
